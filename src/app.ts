05e8236106c1b51de3eb7b4581bbbd33c5bb98e4
devo-ab <per.useravi0@gmail.com>
2024-11-29 18:30:04 +0600

    basic setup done

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+const app: Application = express();
+// const port = 3000;
+
+// parsers
+app.use(express.json());
+app.use(cors());
+// parsers
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+export default app;
